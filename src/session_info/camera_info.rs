b72b6ac9beb1259c89b8a99bda0686cd9b106846
Tim Reed <thetimmyreed@gmail.com>
2024-08-11 19:23:16 -0700

    scenic cameras and deser

--- src/session_info/camera_info.rs
+++ src/session_info/camera_info.rs
@@ -2,21 +2,24 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct CameraInfo {
     pub groups: Vec<CameraGroup>,
 }
 
 #[derive(Debug, Clone, Deserialize, Default, Serialize)]
 #[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct CameraGroup {
     pub group_name: String,
     pub group_num: i32,
-    pub is_scenic: bool,
+    pub is_scenic: Option<bool>,
     pub cameras: Vec<CameraData>,
 }
 
 #[derive(Debug, Clone, Deserialize, Default, Serialize)]
 #[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct CameraData {
     pub camera_name: String,
     pub camera_num: i32,
